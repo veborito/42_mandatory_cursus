065515a7a2f9bcab8ca4acb063b200f2fce06465
Boris Verdecia Echarte <borisverdeciecharte@gmail.com>
2022-07-28 19:20:42 +0200

    strlen fonctionne bien

--- ft_strlen.c
+++ ft_strlen.c
@@ -9,3 +9,19 @@ size_t	ft_strlen(char *s)
 		len++;
 	return (len);
 }
+
+#include <stdio.h>
+
+int	main(int ac, char **av)
+{
+	if (ac > 1)
+	{
+		int	i = 1;
+		while (av[i])
+		{
+			printf("lenght of %s: %lu\n", av[i], ft_strlen(av[i]));
+			i++;
+		}
+	}
+	return (0);
+}
