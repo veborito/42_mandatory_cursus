27c5323847a514f615d0db7a7d94fadcdd6d09d0
Boris Verdecia Echarte <borisverdeciaecharte@gmail.com>
2023-03-13 22:36:47 +0100

    debut d'iteration pour l'algo de tri phase de test

--- rank02/push_swap/src_sort/sort.c
+++ rank02/push_swap/src_sort/sort.c
@@ -6,7 +6,7 @@
 /*   By: bverdeci <bverdeci@42lausanne.ch>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 16:37:19 by bverdeci          #+#    #+#             */
-/*   Updated: 2023/03/12 23:03:09 by bverdeci         ###   ########.fr       */
+/*   Updated: 2023/03/13 22:35:54 by bverdeci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,23 +22,19 @@
 // 	}
 // }
 
-void	ft_sort(t_list **s_a, t_list **s_b, t_result **res)
+void	ft_under_pivot(t_list **s_a, t_list **s_b, t_result **res, int pivot)
 {
-	int			median;
 	int			size;
 	t_num_pos	num_pos;
 	int			i;
 
-	median = ft_lstsize(*s_a) / 2;
-	i = 0;
-	ft_printlst(*s_a);
-	printf("\n");
-	while (i < median - 1)
+	i = ft_lstsize(*s_b);
+	while (i < pivot - 1)
 	{
 		size = ft_lstsize(*s_a);
-		num_pos.num = ft_scan_samller(*s_a, median);
+		num_pos.num = ft_scan_samller(*s_a, pivot);
 		num_pos.pos = ft_getpos(*s_a, num_pos.num);
-		if ((*s_a)->index < median)
+		if ((*s_a)->index < pivot)
 		{
 			ft_lstadd_back_2(res, ft_lstnew_2(ft_push_b(s_a, s_b)));
 			i++;
@@ -48,4 +44,14 @@ void	ft_sort(t_list **s_a, t_list **s_b, t_result **res)
 		else
 			ft_lstadd_back_2(res, ft_lstnew_2(ft_reverse_rotate_a(s_a)));
 	}
+}
+
+void	ft_sort(t_list **s_a, t_list **s_b, t_result **res)
+{
+	int	size_a;
+
+	size_a = ft_lstsize(*s_a);
+	ft_under_pivot(s_a, s_b, res, size_a / 2);
+	ft_under_pivot(s_a, s_b, res, (size_a / 2) + (size_a / 4));
+	ft_printlst(*s_a);
 }
