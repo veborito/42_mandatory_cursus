5299d688e3f462f746316c167384a20511ae9200
Boris Verdecia Echarte <borisverdeciaecharte@gmail.com>
2023-06-08 01:16:21 +0200

    finito reste plus qu'à tester

--- rank02/pipex/src/error.c
+++ rank02/pipex/src/error.c
@@ -1,33 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.h                                            :+:      :+:    :+:   */
+/*   error.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bverdeci <bverdeci@42lausanne.ch>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/02 12:11:03 by bverdeci          #+#    #+#             */
-/*   Updated: 2023/06/07 18:53:00 by bverdeci         ###   ########.fr       */
+/*   Created: 2023/06/07 18:28:31 by bverdeci          #+#    #+#             */
+/*   Updated: 2023/06/07 18:29:36 by bverdeci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PIPEX_H
-# define PIPEX_H
+#include "pipex.h"
 
-# include "libft.h"
-# include <stdio.h>
-# include <sys/wait.h>
-
-// ERROR
-
-void	throw_error(void);
-
-// file descriptors
-
-typedef struct s_fds {
-	int	infile;
-	int	outfile;
-	int	fd[2];
-	int	res;
-}	t_fds;
-
-#endif
+void	throw_error(void)
+{
+	perror(NULL);
+	exit(1);
+}
--- rank02/pipex/src/main.c
+++ rank02/pipex/src/main.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bverdeci <bverdeci@student.42.fr>          +#+  +:+       +#+        */
+/*   By: bverdeci <bverdeci@42lausanne.ch>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/02 12:12:33 by bverdeci          #+#    #+#             */
-/*   Updated: 2023/06/07 17:29:14 by bverdeci         ###   ########.fr       */
+/*   Updated: 2023/06/08 01:10:52 by bverdeci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-char	**get_paths(char **env)
+static char	**get_paths(char **env)
 {
 	char	**paths;
 	char	*temp;
@@ -35,7 +35,7 @@ char	**get_paths(char **env)
 	return (paths);
 }
 
-int	exec_cmd(char **cmds, char **paths, char **env)
+static int	exec_cmd(char **cmds, char **paths, char **env)
 {
 	int		i;
 	char	*path_cmd;
@@ -56,11 +56,11 @@ int	exec_cmd(char **cmds, char **paths, char **env)
 		free(path_cmd);
 	}
 	free(cmd);
-	perror("Command not found ");
+	ft_putendl_fd("Command not found", 2);
 	return (1);
 }
 
-int	pipex(char **av, char **env, int pid)
+static int	process_exec(char **av, char **env, int pid)
 {
 	char	**paths;
 	char	**cmds;
@@ -69,77 +69,71 @@ int	pipex(char **av, char **env, int pid)
 	{
 		cmds = ft_split(av[2], ' ');
 		if (!cmds)
-			exit(1);
+			return (1);
 		paths = get_paths(env);
 		if (!paths)
-			exit(1);
+			return (1);
 		return (exec_cmd(cmds, paths, env));
 	}
 	cmds = ft_split(av[3], ' ');
 	if (!cmds)
-		exit(1);
+		return (1);
 	paths = get_paths(env);
 	if (!paths)
-		exit(1);
+		return (1);
 	if (exec_cmd(cmds, paths, env) == 1)
-		exit(1);
+		return (1);
 	return (0);
+}
+
+static void	pipex(char **av, char **env, t_fds fds, int pid1)
+{
+	if (pid1 == 0)
+	{
+		close(fds.fd[0]);
+		dup2(fds.infile, 0);
+		dup2(fds.fd[1], 1);
+		fds.res = process_exec(av, env, pid1);
+		write(fds.fd[1], &fds.res, sizeof(int));
+		close(fds.fd[1]);
+	}
+	else
+	{
+		close(fds.fd[1]);
+		dup2(fds.fd[0], 0);
+		dup2(fds.outfile, 1);
+		if (read(fds.fd[0], &fds.res, sizeof(int)) == -1)
+			throw_error();
+		if (fds.res == 1)
+			exit(1);
+		process_exec(av, env, pid1);
+		close(fds.fd[0]);
+		wait(NULL);
+	}
 }
 
 int	main(int ac, char **av, char **env)
 {
-	int	fd[2];
-	int	pid1;
-	int	infile;
-	int	outfile;
-	int	res;
+	t_fds	fds;
+	int		pid1;
 
-	pipe(fd);
+	ft_bzero(&fds, sizeof(fds));
 	if (ac != 5)
 	{
 		ft_putendl_fd("Incorrect number of arguments", 2);
 		exit(1);
 	}
-	infile = open(av[1], O_RDONLY, 0777);
-	if (infile < 0)
+	fds.infile = open(av[1], O_RDONLY);
+	if (fds.infile < 0)
+		throw_error();
+	fds.outfile = open(av[4], O_CREAT | O_RDWR, 0777);
+	if (fds.outfile < 0)
 	{
-		perror(NULL);
+		ft_putendl_fd("Can't open or create this file", 2);
 		exit(1);
 	}
-	if (ac == 5)
-	{
-		outfile = open(av[4], O_CREAT | O_RDWR, 0777);
-		if (outfile < 0)
-		{
-			perror(NULL);
-			exit(1);
-		}
-		pid1 = fork();
-		if (pid1 == 0)
-		{
-			close(fd[0]);
-			dup2(infile, 0);
-			dup2(fd[1], 1);
-			res = pipex(av, env, pid1);
-			write(fd[1], &res, sizeof(int));
-			close(fd[1]);
-		}
-		else
-		{
-			close(fd[1]);
-			dup2(fd[0], 0);
-			dup2(outfile, 1);
-			if (read(fd[0], &res, sizeof(int)) == -1)
-			{
-				perror(NULL);
-				exit(1);
-			}
-			if (res == 1)
-				exit(1);
-			pipex(av, env, pid1);
-			close(fd[0]);
-			wait(NULL);
-		}
-	}
+	pipe(fds.fd);
+	pid1 = fork();
+	pipex(av, env, fds, pid1);
 	exit(0);
 }
