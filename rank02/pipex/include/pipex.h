211920c20f988e6bb420cfcfb6eb0cad1c168d80
Boris Verdecia Echarte <bverdeci@c2r1s6.42lausanne.ch>
2023-06-08 17:13:36 +0200

    norme pour pipex

--- rank02/pipex/include/pipex.h
+++ rank02/pipex/include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: bverdeci <bverdeci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/02 12:11:03 by bverdeci          #+#    #+#             */
-/*   Updated: 2023/06/08 16:22:34 by bverdeci         ###   ########.fr       */
+/*   Updated: 2023/06/08 17:12:49 by bverdeci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,6 @@
 
 void	throw_error(char *error);
 void	free_split(char **split);
-
 
 // file descriptors
 
