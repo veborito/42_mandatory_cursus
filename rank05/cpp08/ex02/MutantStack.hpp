5407ea4535c34f58425c3fa081b33a9af728207a
Boris Verdecia Echarte <bverdeci@c2r5s2.42lausanne.ch>
2024-03-19 17:08:43 +0100

    cpp08 fini manque les tests

--- rank05/cpp08/ex02/MutantStack.hpp
+++ rank05/cpp08/ex02/MutantStack.hpp
@@ -2,7 +2,6 @@
 #define MUTANTSTACK_HPP
 
 #include <stack>
-#include <iterator>
 #include <deque>
 
 template < typename T, class container=std::deque<T> >
@@ -10,13 +9,16 @@ class MutantStack : public std::stack<T> {
    public:
 	MutantStack() : std::stack<T>() {}
 	MutantStack(MutantStack const & instance) : std::stack<T>(instance) {}
-	virtual ~MutantStack() {}
+	~MutantStack() {}
 
 	MutantStack<T> &operator=(MutantStack<T> const & rhs) { std::stack<T>::operator=(rhs); return *this; }
 	
-	typedef std::iterator iterator;
+	typedef typename container::iterator iterator;
+    typedef typename container::reverse_iterator reverse_iterator;
 	iterator begin(){ return this->c.begin(); };
 	iterator end(){ return this->c.end(); };
+    reverse_iterator rbegin() { return this->c.rbegin(); };
+    reverse_iterator rend() { return this->c.rend(); };
 };
 
 #endif
